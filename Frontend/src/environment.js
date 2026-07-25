070a05ae00a23d14c7e4e0386fb6b6956a58e409
Vaibhav Tiwari <vt95154@gmail.com>
2026-07-25 23:29:09 +0530

    Remove leading space from server URL

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,9 +1,7 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    " https://apna-video-call-h19t.onrender.com":
-
-    "http://localhost:8000"
-
+    "https://apna-video-call-h19t.onrender.com" :
+    "http://localhost:8000";
 
 export { server };
 export default server;
